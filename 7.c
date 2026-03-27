d834268853449d8dc61f64926e490372d05efbec
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-03-28 02:05:09 +0530

    7th

---
+++ 7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n][n];
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+    
+    int mid = n / 2;
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += a[i][i];             
+        sum += a[i][n - i - 1];     
+        sum += a[mid][i];           
+        sum += a[i][mid];           
+    }
+    sum -= 3 * a[mid][mid];
+    
+    printf("%d\n", sum);
+    return 0;
+}
