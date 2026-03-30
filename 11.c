3f8f850642dff8a12a167499bda66c96523a334a
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-03-30 23:26:50 +0530

    11th

---
+++ 11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+
+        int a[n];
+        for (int i = 0; i < n; i++)
+            scanf("%d", &a[i]);
+
+        // Sort the array
+        qsort(a, n, sizeof(int), cmp);
+
+        int possible = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] - a[i - 1] > 1) {
+                possible = 0;
+                break;
+            }
+        }
+
+        if (possible)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
