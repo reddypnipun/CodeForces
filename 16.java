c3ea6e78e4c86a2e954942011aa2ebf43b45171d
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-04-04 00:08:15 +0530

    16th

---
+++ 16.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class S16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+            
+            if (n != 5) {
+                System.out.println("NO");
+                continue;
+            }
+            
+            char[] arr = s.toCharArray();
+            char[] target = "Timur".toCharArray();
+            
+            Arrays.sort(arr);
+            Arrays.sort(target);
+            
+            if (Arrays.equals(arr, target)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        sc.close();
+    }
+}
