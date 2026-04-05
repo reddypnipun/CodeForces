c606e6cee69031f51ad14f106039cc913f0001c3
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-04-05 15:33:28 +0530

    23rd

---
+++ 23.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int t;
+    int c=0;
+    scanf("%d",&t);
+    while(t--){
+        int x;
+        scanf("%d",&x);
+            printf("%d\n",x);
+    }
+    return 0;
+}
