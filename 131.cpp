27fefb6ffec79995585958a31d5507a4447075e6
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-06-16 22:32:15 +0530

    131st

---
+++ 131.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void s() {
+    long long n;
+    cin >> n;
+    int p[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 22, 11};
+    long long a = p[n % 12];
+    if (a <= n) {
+        cout << a << " " << n - a << "\n";
+    } else {
+        cout << "-1\n";
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            s();
+        }
+    }
+    return 0;
+}
