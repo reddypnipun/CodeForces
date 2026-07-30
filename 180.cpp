dd9e24e8e72fd7d6d46125447baccc32d407c5e7
NIPUNKUMAR REDDY <reddypnipun@gmail.com>
2026-07-31 01:17:09 +0530

    180th

--- 180.cpp
+++ 180.cpp
@@ -1,33 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n, a, b, cnt = 0;
+    int n, a, b;
     if (cin >> n >> a >> b) {
-        vector<char> s(n);
-        for (int i = 0; i < n; i++) cin >> s[i];
-        
+        string s;
+        cin >> s;
+        int c = 0;
         for (int i = 0; i < n; i++) {
             if (s[i] == '.') {
-                if (i + 1 < n && s[i + 1] == '.') {
-                    if (b > a) { s[i] = 'B'; b--; cnt++; }
-                    else { s[i] = 'A'; a--; cnt++; }
-                    
-                    while (i + 1 < n && s[i + 1] == '.') {
-                        if (s[i] == 'B' && a > 0) { s[i + 1] = 'A'; a--; cnt++; }
-                        else if (s[i] == 'A' && b > 0) { s[i + 1] = 'B'; b--; cnt++; }
-                        else break;
-                        i++;
-                    }
+                char p = (i > 0) ? s[i - 1] : '*';
+                if (p == 'A') {
+                    if (b > 0) { s[i] = 'B'; b--; c++; }
+                } else if (p == 'B') {
+                    if (a > 0) { s[i] = 'A'; a--; c++; }
                 } else {
-                    if (a > b && a > 0) { s[i] = 'A'; a--; cnt++; }
-                    else if (b > 0) { s[i] = 'B'; b--; cnt++; }
-                    else if (a > 0) { s[i] = 'A'; a--; cnt++; }
+                    if (a >= b && a > 0) { s[i] = 'A'; a--; c++; }
+                    else if (b > a && b > 0) { s[i] = 'B'; b--; c++; }
                 }
             }
         }
-        cout << cnt << "\n";
+        cout << c << "\n";
     }
     return 0;
 }
